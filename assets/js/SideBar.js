2d451f9498a4c7f8c251a8c9529eb70ae8984ed7
Yutty Akiko <yutty.akiko@gmail.com>
2023-09-28 02:06:07 -0300

    Ajuste do Mozão S2 no menu sidebar

--- assets/js/SideBar.js
+++ assets/js/SideBar.js
@@ -1,11 +1,11 @@
 function hideMenu(x) {
-  let menu = document.getElementById('hide-menu');
+  let menu = document.querySelector('.menu');
 
-  x.classList.toggle('change');
+  x.classList.toggle('change'); // Alterna a classe 'change' no botão para efeito visual
 
-  if (menu.style.display == 'flex') {
-    menu.style.display = 'none';
+  if (menu.classList.contains('hide-menu')) { // Verifica se o elemento do menu contém a classe 'hide-menu'
+    menu.classList.remove('hide-menu'); // Remove a classe 'hide-menu' para mostrar o menu
   } else {
-    menu.style.display = 'flex';
+    menu.classList.add('hide-menu'); // Adiciona a classe 'hide-menu' para ocultar o menu
   }
 }
