1d7963e74155a31ab7168df12d076f069dc63a11
Yutty Akiko <yutty.akiko@gmail.com>
2023-09-28 04:42:09 -0300

    Ajuste o smooth no app.js para funcionar em todos os navegadores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // Buscando os links do menu de navegação
-const menuItems = document.querySelectorAll('.menu a');
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
 
 menuItems.forEach(item => {
   item.addEventListener('click', scrollToIdOnClick);
@@ -12,15 +12,49 @@ function getScrollTopByHref(element) {
 
 function scrollToIdOnClick(event) {
   event.preventDefault(); // Previnindo ações padrões
-  const section = getScrollTopByHref(event.target) - 90; // Busca o alvo clicado e atribui na função
-
+  const section = getScrollTopByHref(event.currentTarget) - 90; // Busca o alvo clicado e atribui na função
   scrollToPosition(section);
 }
 
 function scrollToPosition(section) {
-  window.scroll({
+  smoothScrollTo(0, section);
+  /* window.scroll({
     // Para onde quero dar o scrool
     top: section,
     behavior: 'smooth',
-  }); 
+  }); */
 }
+
+// Caso deseje suporte a browsers antigos / que não suportam scroll smooth nativo
+/* 
+ * Smooth scroll animation
+ * @param {int} endX: destination x coordinate
+ * @param {int) endY: destination y coordinate
+ * @param {int} duration: animation duration in ms
+ */
+// Pegueis estas funções prontas
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  // Easing function
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60); // 60 fps
+};
