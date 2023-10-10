fc0d242996f5c01c7503351594f8aa00bd2c01d7
Yutty Akiko <yutty.akiko@gmail.com>
2023-10-10 20:51:41 -0300

    update

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,6 +1,6 @@
 // Seção onde vão ficar os cards
 let sectionSkills = document.querySelector('.cards-container-skills');
-let sectionTools = document.querySelector('.carousel');
+let sectionTools = document.querySelector('.slider');
 
 // Endereço do arquivo JSON
 const url = 'cards.json';
